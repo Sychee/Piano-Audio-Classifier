50c0ea76d329f94b262cf1f2fc69ad5d7cc41263
Gautam Banuru <32660502+gbanuru18@users.noreply.github.com>
2020-02-17 21:35:16 +0000

    updated getSpectrogram() and getClasses()
    getSpectrogram()  normalizes data between 0-255 instead of 0-1,
    getClasses() gives number of classes and data pieces.

--- dataSorting.py
+++ dataSorting.py
@@ -5,18 +5,24 @@ from collections import defaultdict
 import cv2
 import split_folders
 
-def countClasses(root): #counts the number of classes and data within those classes
-    countClass = 0
-    countData = 0
+def countClasses(root, debugContent=False): #counts the classes and number of data from root
+    class_count = 0
+    data_count = 0
     for dataClass in os.listdir(root):
-        countClass +=1
+        class_count +=1
         for dataItem in os.listdir(root + "/" + dataClass):
-            countData += 1
+            data_count += 1
+        
+        if debugContent:
+            img = cv2.imread(root + "/" + dataClass + "/" + dataItem, cv2.IMREAD_GRAYSCALE)
+            strImg = str(','.join(str(item) for innerlist in img for item in innerlist))
+            print("DataClass: ", dataClass)
+            print("Img String", strImg)
             
-    print(f"Total Classes: {countClass}")
-    print(f"Total Data: {countData}")
+    print(f"Total Classes: {class_count}")
+    print(f"Total Data: {data_count}")
 
-def InitiateData(root): #clears all data in root folder and creates all/all folder
+def InitiateData(root): #clears all data in root folder
     for item in os.listdir(root):
         assert item in ["all", "train", "validation", "test", ""] #safety check to make sure root is correct
         shutil.rmtree(os.path.join(root,item)) #clears all items in root
@@ -29,17 +35,25 @@ def populateData(filtered_performances, root, save=True):
     """
     Slices spectrograms in filtered_performances and saves images in all/all folder with class as folder name
     """
+    
     if save:
         InitiateData(root) #clear data in root folder
+        
     root = os.path.join(root, "all/all")
     class_count = defaultdict(int)
     data_count = 0
     for piecenum in range(len(filtered_performances)):
-        piece = filtered_performances[piecenum]
-        performance = piece.load_performance(piece.available_performances[0], require_audio=False)
-        spectrogram = performance.load_spectrogram()
-        slices = spectrogram.shape[1]
         print(f"Piece {piecenum} of {len(filtered_performances)}")
+        
+        piece = filtered_performances[piecenum]
+        try:
+            performance = piece.load_performance(piece.available_performances[0], require_audio=False)
+            spectrogram = performance.load_spectrogram()
+        except Exception as e:
+            print(f"EXCEPTION at Piece {piecenum}: {e}")
+            continue
+            
+        slices = spectrogram.shape[1]
         for slice in range(slices):
             try:
                 trueVal = str(int(''.join(map(str, convFilter.getNvec(slice, performance))), 2))
@@ -49,16 +63,16 @@ def populateData(filtered_performances, root, save=True):
                     class_count[trueVal] = 1
                 data_count += 1
                 
+                trueSpec = convFilter.getSpectrogram(slice, performance)
+                
                 if save:
-                    trueSpec = convFilter.getSpectrogram(slice, performance)
                     addImageToDirectory(trueSpec, f"img{class_count[trueVal]}.png", trueVal, root)
                 
             except IndexError as e:
-                print(f"IndexError: PieceNum: {piecenum}, Slice: {slice}, Message: {e}")    
-
-    print(f"Classes: {len(class_count)}")
-    print(f"Data Count: {data_count}")
-    #print(f"All Classes: {sorted(class_count.items(), key=lambda k_v: k_v[1], reverse=True)}")
+                print(f"INDEXERROR: PieceNum: {piecenum}, Slice: {slice}, Message: {e}")    
+    
+    print(f"Total Classes: {len(class_count)}")
+    print(f"Total Data: {data_count}")
             
 def addImageToDirectory(image, imageName, folder, root):
     """
@@ -95,12 +109,10 @@ def divideDataIntoTrainValTestSets(root, train=.6, val=.2, test=.2):
         os.rename(os.path.join(root, item + temp), os.path.join(root, item))
 
 if __name__ == "__main__":
-    # path to MSMD data set
-    DATA_ROOT_MSMD = '/Users/gbanuru/PycharmProjects/HACKUCI/msmd_aug_v1-1_no-audio/'
-    dataRoot = "/Users/gbanuru/PycharmProjects/HACKUCI/msmd/tutorials/data_root"
+    DATA_ROOT_MSMD = '/Users/gbanuru/PycharmProjects/HACKUCI/msmd_aug_v1-1_no-audio/' # path to MSMD data set
+    dataRoot = "/Users/gbanuru/PycharmProjects/HACKUCI/msmd/tutorials/data_root" # path to our created dataset
 
-    filtered_performances = convFilter.filteredData(DATA_ROOT_MSMD) #creates a list with piece object
-    populateData(filtered_performances[:50], dataRoot, save=False) #store data from 50 performances at data_root
-    divideDataIntoTrainValTestSets(dataRoot) #divide data from data_root into train, validation, test sets
-
-    countClasses(dataRoot + "/all/all")
+    filtered_performances = convFilter.filteredData(DATA_ROOT_MSMD) # creates a list with piece object
+    populateData(filtered_performances[:50], dataRoot, save=True) # store data from 50 performances at data_root
+    divideDataIntoTrainValTestSets(dataRoot) # divide data from data_root into train, validation, test sets
+    countClasses(dataRoot + "/all/all") # count number of classes and data pieces
